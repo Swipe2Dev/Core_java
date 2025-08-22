23432681f3947f172ae6b5830bb2e503e88e5b07
Kausalya-s673 <164292535+Kausalya-s673@users.noreply.github.com>
2025-08-22 18:58:20 +0530

    Created Square.java

---
+++ src/Methods/square.java
@@ -0,0 +1,29 @@
+package Methods;
+
+public class square {
+
+	public static int squ(int num) {
+		return num * num;
+	}
+
+	public static void main(String[] args) {
+		
+		int num = 7;
+		squ(num);
+		System.out.println(squ(num));
+	}
+}
+//package Methods;
+//
+//public class square {
+//
+//	public static double squ(int num) {
+//		return Math.pow(num, 2);
+//	}
+//
+//	public static void main(String[] args) {
+//				int num = 9;
+//		squ(num);
+//		System.out.println(squ(num));
+//	}
+//}
