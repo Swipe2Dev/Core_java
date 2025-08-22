f7a290d821bd8b4e6582aeee3af5b9f0c433b1fa
Kausalya-s673 <164292535+Kausalya-s673@users.noreply.github.com>
2025-08-22 18:26:17 +0530

    initial

---
+++ src/Methods/problem_1.java
@@ -0,0 +1,30 @@
+//package Methods;
+
+//public class problem_1 {
+//	
+//	static void function() {
+//		System.out.println("Hello World !");
+//	}
+//
+//	public static void main(String[] args) {
+//	function();
+//
+//	}
+//
+//}
+//
+package Methods;
+
+public class problem_1 {
+	
+	 void function() {
+		System.out.println("Hello World !!");
+	}
+
+	public static void main(String[] args) {
+		problem_1 p=new problem_1();
+	p.function();
+
+	}
+
+}
